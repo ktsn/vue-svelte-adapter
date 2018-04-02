5664c278d5d2a093459d9c73f03fcbe3517c7536
ktsn <ktsn55@gmail.com>
2018-04-02 14:28:26 +0900

    feat: enable to pass props

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import Vue, { VueConstructor } from 'vue'
+import Vue, { VueConstructor, PropOptions } from 'vue'
 
-export function toVue(Ctor: any): VueConstructor {
-  return Vue.extend({
+export type Prop<T> = { (): T } | { new (...args: any[]): T & object }
+
+export type PropValidator<T> = PropOptions<T> | Prop<T> | Prop<T>[]
+
+export type RecordPropsDefinition<T> = { [K in keyof T]: PropValidator<T[K]> }
+
+export type ArrayPropsDefinition<T> = (keyof T)[]
+
+export type PropsDefinition<T> =
+  | ArrayPropsDefinition<T>
+  | RecordPropsDefinition<T>
+
+export interface AdapterOptions<Props> {
+  props?: PropsDefinition<Props>
+}
+
+export function toVue<Props>(
+  Ctor: any,
+  options: AdapterOptions<Props> = {}
+): VueConstructor<Props & Vue> {
+  return Vue.extend<Vue, {}, {}, Props>({
+    props: options.props as any,
+
     mounted() {
-      ;(this as any).instance = new Ctor({
-        target: this.$el
-      })
+      const instance = ((this as any).instance = new Ctor({
+        target: this.$el,
+        data: this.$props
+      }))
+
+      const propKeys = Object.keys(this.$props || {})
+      let prevProps = copy(this.$props || {})
+
+      if (propKeys.length > 0) {
+        this.$watch(() => {
+          const target: Record<string, any> = {}
+
+          propKeys.forEach(key => {
+            const value = this.$props[key]
+            if (
+              prevProps[key] !== value ||
+              (value !== null && typeof value === 'object')
+            ) {
+              target[key] = value
+            }
+          })
+
+          if (Object.keys(target).length > 0) {
+            instance.set(target)
+            prevProps = copy(this.$props || {})
+          }
+        }, noop)
+      }
     },
 
     beforeDestroy() {
@@ -17,4 +63,15 @@ export function toVue(Ctor: any): VueConstructor {
       return h('div')
     }
   })
+}
+
+function noop(): void {
+  // do nothing
+}
+
+function copy<T extends object>(value: T): T {
+  return Object.keys(value).reduce<any>((acc, key) => {
+    acc[key] = (value as any)[key]
+    return acc
+  }, {})
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,75 @@
+import Vue, { VNode } from 'vue'
 import { mount } from '@vue/test-utils'
 import { toVue } from '../src/index'
 import Basic from './fixtures/Basic.html'
+import Data from './fixtures/Data.html'
+import ObjectData from './fixtures/ObjectData.html'
 
 describe('Vue Svelte Adapter', () => {
   it('renders template', () => {
     const BasicVue = toVue(Basic)
     const wrapper = mount(BasicVue)
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('ports Vue props to Svelte data', () => {
+    const DataVue = toVue(Data, {
+      props: {
+        message: String
+      }
+    })
+    const wrapper = mount(DataVue, {
+      propsData: {
+        message: 'Test Message'
+      }
+    })
+    expect(wrapper.text()).toBe('Message: Test Message')
+  })
+
+  it('updates Svelte data when props are updated', () => {
+    const DataVue = toVue(Data, {
+      props: {
+        message: String
+      }
+    })
+    const wrapper = mount(DataVue, {
+      propsData: {
+        message: 'Test'
+      }
+    })
+    wrapper.setProps({
+      message: 'Updated'
+    })
+    expect(wrapper.text()).toBe('Message: Updated')
+  })
+
+  it('updates Svelte data when object props are mutated', () => {
+    const DataVue = toVue(ObjectData, {
+      props: {
+        user: Object
+      }
+    })
+    const Wrapper = Vue.extend({
+      data() {
+        return {
+          user: {
+            name: 'Foo'
+          }
+        }
+      },
+
+      render(h): VNode {
+        return h(DataVue, {
+          props: {
+            user: this.user
+          }
+        })
+      }
+    })
+    const wrapper = mount(Wrapper)
+
+    expect(wrapper.text()).toBe('Name: Foo')
+    wrapper.vm.user.name = 'Bar'
+    expect(wrapper.text()).toBe('Name: Bar')
   })
 })
